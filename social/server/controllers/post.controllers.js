d85d68432dd38fc012c0b1c63f1f9f58d225eef7
Godatcode <arkaghosh25012005@gmail.com>
2025-10-03 15:50:31 +0530

    Comment section working expectedly

--- social/server/controllers/post.controllers.js
+++ social/server/controllers/post.controllers.js
@@ -65,10 +65,9 @@ export const uploadPost = async (req, res) => {
 };
 export const getAllPosts = async (req, res) => {
   try {
-    const posts = await Post.find({}).populate(
-      "author",
-      "userName profileImage"
-    );
+    const posts = await Post.find({})
+      .populate("author", "userName profileImage")
+      .populate("comments.user", "userName profileImage");
     return res.status(200).json(posts);
   } catch (error) {
     return res.status(404).json({ message: "No Posts Found" });
@@ -113,9 +112,28 @@ export const like = async (req, res) => {
 
 
 export const comment  = async(req , res)=>{
-   // postid
-   // userid
-   // userName
-   // text
-   // createdAt
+   try {
+     const postId = req.params.postId;
+     const { text } = req.body;
+
+     if (!text || !text.trim()) {
+       return res.status(400).json({ message: "Comment text is required" });
+     }
+
+     const post = await Post.findById(postId);
+     if (!post) {
+       return res.status(404).json({ message: "No post Found" });
+     }
+
+     post.comments.push({ user: req.userId, text: text.trim() });
+     await post.save();
+
+     const populatedPost = await Post.findById(post._id)
+       .populate("author", "userName profileImage")
+       .populate("comments.user", "userName profileImage");
+
+     return res.status(201).json(populatedPost);
+   } catch (error) {
+     return res.status(500).json({ message: "Failed to add comment" });
+   }
 }
