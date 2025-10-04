ead18e02812ace9dfdd297d9028eaa702f547c38
Piyush Kumar Mahato <piyush6805@gmail.com>
2025-10-04 21:51:37 +0530

    Added comment in post.controllers.js

--- social/server/routes/post.routes.js
+++ social/server/routes/post.routes.js
@@ -1,17 +1,18 @@
-import express from 'express'
-import { uploadPost , getAllPosts  , like} from '../controllers/post.controllers.js'
-import { upload } from '../middlewares/multer.js'
-import isAuth from '../middlewares/isAuth.js'
+import express from "express";
+import {
+  uploadPost,
+  getAllPosts,
+  like,
+  comment,
+} from "../controllers/post.controllers.js";
+import { upload } from "../middlewares/multer.js";
+import isAuth from "../middlewares/isAuth.js";
 
+const postRouter = express.Router();
 
+postRouter.post("/uploadPost", isAuth, upload.single("mediaUrl"), uploadPost);
+postRouter.get("/getAllPosts", isAuth, getAllPosts);
+postRouter.post("/like/:postId", isAuth, like);
+postRouter.post("/comment/:postId", isAuth, comment);
 
-const postRouter = express.Router()
-
-
-postRouter.post('/uploadPost',isAuth , upload.single('mediaUrl'), uploadPost )
-postRouter.get("/getAllPosts" , isAuth ,getAllPosts );
-postRouter.post("/like/:postId", isAuth, like)
-
-
-
-export default postRouter
+export default postRouter;
