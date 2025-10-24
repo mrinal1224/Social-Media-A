ab52a88c665c01bf35fb8d12fb890771869f0dc5
Sakshi <sakshishah049@gmail.com>
2025-10-24 18:30:16 +0530

    comment feature added

--- social/server/routes/post.routes.js
+++ social/server/routes/post.routes.js
@@ -1,17 +1,18 @@
 import express from 'express'
-import { uploadPost , getAllPosts  , like} from '../controllers/post.controllers.js'
+import { uploadPost, getAllPosts, like, comment, editComment, deleteComment } from '../controllers/post.controllers.js'
 import { upload } from '../middlewares/multer.js'
 import isAuth from '../middlewares/isAuth.js'
-
 
 
 const postRouter = express.Router()
 
 
-postRouter.post('/uploadPost',isAuth , upload.single('mediaUrl'), uploadPost )
-postRouter.get("/getAllPosts" , isAuth ,getAllPosts );
+postRouter.post('/uploadPost', isAuth, upload.single('mediaUrl'), uploadPost)
+postRouter.get("/getAllPosts", isAuth, getAllPosts);
 postRouter.post("/like/:postId", isAuth, like)
-
+postRouter.post('/comment/:postId', isAuth, comment);
+postRouter.put('/comment/:postId/:commentId', isAuth, editComment);
+postRouter.delete('/comment/:postId/:commentId', isAuth, deleteComment);
 
 
 export default postRouter
