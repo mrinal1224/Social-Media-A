f4dc8e624906f0c45bd6848ed010af5bbdf3aa16
Mrinal Bhattacharya <iammrinalkc@gmail.com>
2026-09-22 10:28:49 +0530

    Complete reels flow: register reel routes

--- social/server/index.js
+++ social/server/index.js
@@ -4,31 +4,35 @@ dotenv.config();
 import connectDB from "./config/db.js";
 import authRouter from "./routes/auth.routes.js";
 import userRouter from "./routes/user.routes.js";
-import postRouter from './routes/post.routes.js'
+import postRouter from "./routes/post.routes.js";
 import followRouter from "./routes/followers.routes.js";
 import cookieParser from "cookie-parser";
 import storyRouter from "./routes/story.routes.js";
-import cors from 'cors'
+import reelRouter from "./routes/reel.routes.js";
+import cors from "cors";
 
 const app = express();
 const PORT = 8000;
-// Middlewares
+
+// Existing middleware configuration.
 app.use(
   cors({
     origin: "http://localhost:5173",
-    credentials: true
+    credentials: true,
   })
 );
 app.use(cookieParser());
 app.use(express.json());
-app.use(express.urlencoded())
+app.use(express.urlencoded());
 
-// Authentication routes
 app.use("/api/auth", authRouter);
 app.use("/api/user", userRouter);
 app.use("/api/post", postRouter);
 app.use("/api/follow", followRouter);
 app.use("/api/story", storyRouter);
+
+// Added reel routes so Home can fetch and upload reels through the API.
+app.use("/api/reel", reelRouter);
 
 connectDB();
 
