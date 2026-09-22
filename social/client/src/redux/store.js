7af6e8d0be113794dc315800d09a723dde0cd2ca
Mrinal Bhattacharya <iammrinalkc@gmail.com>
2026-09-22 10:29:11 +0530

    Complete reels GET flow: store.js

--- social/client/src/redux/store.js
+++ social/client/src/redux/store.js
@@ -1,14 +1,16 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userSlice from "./userSlice";
-import postSlice from './postSlice'
-import storySlice from './storySlice'
+import postSlice from "./postSlice";
+import storySlice from "./storySlice";
+import reelSlice from "./reelSlice";
 
-
+// Added reel state so Home can consume the reel GET response globally.
 const store = configureStore({
   reducer: {
     user: userSlice,
-    post:postSlice,
-    story:storySlice
+    post: postSlice,
+    story: storySlice,
+    reel: reelSlice,
   },
 });
 
